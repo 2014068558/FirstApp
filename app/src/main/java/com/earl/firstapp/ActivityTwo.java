e9b97e28aa1f286eef10e22e9873099d85f7036b
Earl Rada <2014068558@ust-ics.mygbiz.com>
2017-09-05 22:23:11 +0800

    First Commit

---
+++ app/src/main/java/com/earl/firstapp/ActivityTwo.java
@@ -0,0 +1,14 @@
+package com.earl.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
